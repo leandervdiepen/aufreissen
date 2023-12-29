6fce142884bbb06c0d356199006d557101bca06e
Leander van Diepen <leander.vandiepen@check24.de>
2023-12-29 14:34:17 +0100

    setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="App">
-        <header className="App-header">
-          <img src={reactLogo} className="App-logo" alt="logo" />
-          <img src={viteLogo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-        </header>
         <main>
-          <p className="counter">
-            <button
-              type="button"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              count is: {count}
-            </button>
-          </p>
+          <h1 className="text-green-500">Hello, world!</h1>
+          <div className="wrapper">
+            <div className="markdown">
+              <h2>Markdown Textarea</h2>
+            </div>
+
+            <div className="preview">
+              <h2>Preview</h2>
+            </div>
+          </div>
         </main>
       </div>
     </>
